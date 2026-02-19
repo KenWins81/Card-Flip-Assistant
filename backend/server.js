176bf26545cefce84373af428cf27c1c775dc31a
KenWins81 <kendell3000@gmail.com>
2026-02-18 19:06:02 -0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,11 @@ const nodemailer = require('nodemailer');
 require('dotenv').config();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PATCH', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(express.json());
 
 // Configuration
